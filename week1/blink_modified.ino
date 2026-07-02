bf1ed7c77c024bd0abda2aa88eb4c0734b59bcf2
Vidhika Vilas Dhurve <vidhikavdhurve.27@gmail.com>
2026-07-02 17:59:33 +0530

    add 3-fast-blink modified in blink_modified.ino
    feat: add 3-fast-blink modified starter sketch.

---
+++ week1/blink_modified.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // 3 Fast Blinks (100ms ON, 100ms OFF)
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+    delay(100);
+  }
+
+  delay(2000); // 2 Second Pause
+}
