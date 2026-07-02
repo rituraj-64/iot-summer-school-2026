98045bd6ca6d3e9fe88f8a181c662b89827d1dae
Ritu Raj <sahilrajritu13@gmail.com>
2026-07-02 14:43:59 +0530

    feat: add basic LED blink at 500ms interval

---
+++ week1/led_blink/led_blink.ino
@@ -0,0 +1,12 @@
+const uint8_t LED_PIN = 13;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
